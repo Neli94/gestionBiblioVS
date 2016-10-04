c3e2c51413f119afa6f5f2a5e605271b06658790
Curso <Curso@FORMACION-TO111>
2016-10-04 15:34:07 +0200

    bla

--- EjemploWebForm/Global.asax.cs
+++ EjemploWebForm/Global.asax.cs
@@ -10,7 +10,7 @@ namespace EjemploWebForm
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
-        {
+        { 
         }
     }
 }
